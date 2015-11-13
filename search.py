834ebebcc43b6edf6fe3694c0337b4d2d69751f2
rresma2 <zantes797@icloud.com>
2015-11-12 18:59:39 -0800

    ucs moved

---
+++ search.py
@@ -0,0 +1,86 @@
+import heapq
+
+# Data structure for supporting uniform cost search.
+class PriorityQueue:
+    def  __init__(self):
+        self.DONE = -100000
+        self.heap = []
+        self.priorities = {}  # Map from state to priority
+
+    # Insert |state| into the heap with priority |newPriority| if
+    # |state| isn't in the heap or |newPriority| is smaller than the existing
+    # priority.
+    # Return whether the priority queue was updated.
+    def update(self, state, newPriority):
+        oldPriority = self.priorities.get(state)
+        if oldPriority == None or newPriority < oldPriority:
+            self.priorities[state] = newPriority
+            heapq.heappush(self.heap, (newPriority, state))
+            return True
+        return False
+
+    # Returns (state with minimum priority, priority)
+    # or (None, None) if the priority queue is empty.
+    def removeMin(self):
+        while len(self.heap) > 0:
+            priority, state = heapq.heappop(self.heap)
+            if self.priorities[state] == self.DONE: continue  # Outdated priority, skip
+            self.priorities[state] = self.DONE
+            return (state, priority)
+        return (None, None) # Nothing left...
+
+class UniformCostSearch():
+    def __init__(self, verbose=0):
+        self.verbose = verbose
+
+    def solve(self, problem):
+        # If a path exists, set |actions| and |totalCost| accordingly.
+        # Otherwise, leave them as None.
+        self.actions = None
+        self.totalCost = None
+        self.numStatesExplored = 0
+        self.finalState = None
+
+        # Initialize data structures
+        frontier = PriorityQueue()  # Explored states are maintained by the frontier.
+        backpointers = {}  # map state to (action, previous state)
+
+        # Add the start state
+        startState = problem.start_state()
+        frontier.update(startState, 0)
+
+        while True:
+            # Remove the state from the queue with the lowest pastCost
+            # (priority).
+            state, pastCost = frontier.removeMin()
+            if state == None: break
+            self.numStatesExplored += 1
+            if self.verbose >= 2:
+                print "Exploring %s with pastCost %s" % (state, pastCost)
+
+            # Check if we've reached the goal; if so, extract solution
+            if problem.is_goal(state):
+                self.finalState = state
+                self.actions = []
+                while state != startState:
+                    action, prevState = backpointers[state]
+                    self.actions.append(action)
+                    state = prevState
+                self.actions.reverse()
+                self.totalCost = pastCost
+                if self.verbose >= 1:
+                    print "numStatesExplored = %d" % self.numStatesExplored
+                    print "totalCost = %s" % self.totalCost
+                    print "actions = %s" % self.actions
+                return
+
+            # Expand from |state| to new successor states,
+            # updating the frontier with each newState.
+            for action, newState, cost in problem.succ_and_cost(state):
+                if self.verbose >= 3:
+                    print "  Action %s => %s with cost %s + %s" % (action, newState, pastCost, cost)
+                if frontier.update(newState, pastCost + cost):
+                    # Found better way to go to |newState|, update backpointer.
+                    backpointers[newState] = (action, state)
+        if self.verbose >= 1:
+            print "No path found"
