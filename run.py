39e89d47ff2d7a2a655f2e985f05880025f3eafc
rresma2 <zantes797@icloud.com>
2015-12-02 20:25:23 -0800

    changes to run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import sys
 import util
 import search
 import songwriter
@@ -19,37 +20,14 @@ import random
 # ucs.solve(w)
 # lyrics =  ucs.finalState.lyrics
 
-fo = open("songs.txt", "w")
-fo.seek(0)
-fo.truncate()
-fo.write("CS 221 Songwriter AI\n")
-fo.write("By: Alex Wells, Rob Resma, Kyle Qian\n\n\n")
-fo.close()
-
-numSongs = 10
-genres = ["country", "hiphoprap", "pop"]
-ctDict = Counter()
-
-for i in range(numSongs):
-	genre = random.choice(genres)
-	ctDict[genre] += 1
-	fileName = "songs.txt"
-	fo = open(fileName, "a")
+def outputSong(genre):
 	ucs = search.UniformCostSearch(0)
 	w = songwriter.SongWriter(genre)
 	ucs.solve(w)
 	lyrics =  ucs.finalState.lyrics
-
-	"""
-	Putting the lyrics that are calculated in a
-	readable format to be saved in a text file.
-	"""
-
-	ct = ctDict[genre]
-	genre = genre.capitalize()
-	fo.write("%s Song %s\n\n" % (genre,ct))
-
 	newLines = []
+	print "========================================================="
+	print "Here's a song for you. I hope you like it!\n\n"
 	for line in lyrics:
 		newLine = []
 		for word in line:
@@ -58,21 +36,78 @@ for i in range(numSongs):
 
 	for line in newLines:
 		print line
-		fo.write(line)
-		fo.write("\n")
+	print "========================================================="
 
-	fo.write("\n\n\n\n")
+def writeSongs(numSongs, fileName):
+	fo = open("songs.txt", "w")
+	fo.seek(0)
+	fo.truncate()
+	fo.write("CS 221 Songwriter AI\n")
+	fo.write("By: Alex Wells, Rob Resma, Kyle Qian\n\n\n")
 	fo.close()
 
+	numSongs = 10
+	genres = ["country", "hiphoprap", "pop"]
+	ctDict = Counter()
+
+	for i in range(numSongs):
+		genre = random.choice(genres)
+		ctDict[genre] += 1
+		fileName = "songs.txt"
+		fo = open(fileName, "a")
+		ucs = search.UniformCostSearch(0)
+		w = songwriter.SongWriter(genre)
+		ucs.solve(w)
+		lyrics =  ucs.finalState.lyrics
+
+		"""
+		Putting the lyrics that are calculated in a
+		readable format to be saved in a text file.
+		"""
+
+		ct = ctDict[genre]
+		genre = genre.capitalize()
+		fo.write("%s Song %s\n\n" % (genre,ct))
+
+		newLines = []
+		for line in lyrics:
+			newLine = []
+			for word in line:
+				newLine.append(str(word))
+			newLines.append(" ".join(newLine))
+
+		for line in newLines:
+			print line
+			fo.write(line)
+			fo.write("\n")
+
+		fo.write("\n\n\n\n")
+		fo.close()
+
+def main(argv):
+	if argv is not None:
+		opt = argv[0]
+		if opt == "-s":
+			if len(argv) != 3:
+				print("Error: Please specify the arguments in the form of: -s 'fileName.txt' n (number of songs)")
+				return
+			fileName = argv[1]
+			numSongs = argv[2]
+			writeSongs(numSongs, fileName)
+			return
+		elif opt == "-o":
+			if len(argv) != 2:
+				print ("Error: Please specify the arguments in the form of: -o 'genre' (hiphoprap,country,or pop)")
+				return
+			genre = argv[1]
+			outputSong(genre)
+	else:
+		print("Error: Please use -o or -s...")
 
 
+		
 
-
-
-
-
-
-
-
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
 
