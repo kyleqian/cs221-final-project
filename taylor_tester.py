7ca4977049fe9fc7f3c5a3b7730c6c3273d3e4ce
Kyle Qian <cqian@stanford.edu>
2015-12-11 00:46:40 -0800

    added baseline feature

--- taylor_tester.py
+++ taylor_tester.py
@@ -4,17 +4,22 @@ import search
 import random
 import os
 import json
+import sys
 from datetime import datetime
 
 class TaylorTester():
-	def __init__(self, ucs):
+	def __init__(self, ucs, baseline_flag=False, true_random=True):
+		self.BASELINE_FLAG = baseline_flag
 		self.NUM_SAMPLES = 3
 		self.PERCENT_OF_LINES_TESTED = 20
-		self.TRUE_RANDOM = True
 		self.GENRE = 'hiphoprap'
 		self.ucs = ucs
 		self.taylor_lyrics = defaultdict(list)
+		self.corpus = set()
 		self.__read_lyrics()
+		self.random = random
+		if not true_random:
+			self.random.seed(42)
 
 	def evaluate(self):
 		num_correct = 0
@@ -27,7 +32,7 @@ class TaylorTester():
 		# list of (lyric matrix, list of (line number, word index))
 		filled_songs = self.__fill_songs(unfilled_songs)
 
-		for filled_song, fill_choices in filled_songs:
+		for filled_song,fill_choices in filled_songs:
 			for l,i in fill_choices:
 				line = list(filled_song[l])
 				line[i] = '[%s]' % line[i]
@@ -51,7 +56,9 @@ class TaylorTester():
 
 		### logging
 		score = (100.0 * num_correct / num_examples)
-		with open(os.path.dirname(os.path.realpath(__file__)) + '/logs/' + str(datetime.now()) + '.txt', 'w') as f:
+		header = 'baseline-' if self.BASELINE_FLAG else 'ai-'
+		path = '%s/logs/%s%s.txt' % (os.path.dirname(os.path.realpath(__file__)), header, str(datetime.now()))
+		with open(path, 'w') as f:
 			for x in results:
 				f.write(x)
 			f.write('\n')
@@ -69,20 +76,13 @@ class TaylorTester():
 			lyrics = json.loads(f.read())
 			for song in lyrics:
 				for line in lyrics[song]:
-					self.taylor_lyrics[song].append(line.split())
-
-		# curr_song = ''
-		# for l in open('taylor-lyrics.txt', 'r'):
-		# 	line = l.strip().split()
-		# 	if len(line) > 0:
-		# 		if line[0] == '###':
-		# 			curr_song = ' '.join(line[1:])
-		# 		else:
-		# 			self.taylor_lyrics[curr_song].append(line)
+					list_of_words = line.split()
+					self.taylor_lyrics[song].append(list_of_words)
+					if self.BASELINE_FLAG:
+						for word in list_of_words:
+							self.corpus.add(word)
 
 	def __generate_unfilled_songs(self):
-		if not self.TRUE_RANDOM: random.seed(42)
-
 		results = []
 		titles = random.sample(self.taylor_lyrics.keys(), self.NUM_SAMPLES)
 		for t in titles:
@@ -100,16 +100,28 @@ class TaylorTester():
 			results.append((lyrics_matrix, tuples_of_lines_and_indices))
 		return results
 
+	# lyrics is a list of lists containing individual words
 	def __fill_songs(self, unfilled_songs):
 		results = []
-		for unfilled_song,fill_list in unfilled_songs:
-			sw = songwriter.SongWriter(self.GENRE, unfilled_song)
-			self.ucs.solve(sw)
-			if ucs.finalState.lyrics is not None:
-				filled_song = ucs.finalState.lyrics
-			else:
-				print "No solution"
-			results.append((filled_song, fill_list))
+
+		if not self.BASELINE_FLAG:
+			for unfilled_song,fill_list in unfilled_songs:
+				sw = songwriter.SongWriter(self.GENRE, unfilled_song)
+				self.ucs.solve(sw)
+				if ucs.finalState.lyrics is not None:
+					filled_song = ucs.finalState.lyrics
+				else:
+					print "No solution"
+				results.append((filled_song, fill_list))
+
+		else:
+			for unfilled_song,fill_list in unfilled_songs:
+				new_song = list(unfilled_song)
+				for line_number,word_index in fill_list:
+					random_word = self.random.sample(self.corpus, 1)
+					new_song[line_number][word_index] = list(random_word)[0]
+				results.append((new_song, fill_list))
+
 		return results
 
 	def print_lyrics(self, title):
@@ -117,7 +129,8 @@ class TaylorTester():
 			print ' '.join(line)
 
 if __name__ == '__main__':
+	baseline_flag = True if len(sys.argv) > 1 and sys.argv[1] == '-b' else False
 	ucs = search.UniformCostSearch(0)
-	t = TaylorTester(ucs)
+	t = TaylorTester(ucs, baseline_flag)
 	t.evaluate()
 	# t.print_lyrics('You Belong With Me')
